e301b4fc3e91ca1d4d8f7b1b10d01ce82f450f4b
Benjamin Wasty <benjamin.wasty@softwarediagnostics.com>
2014-02-21 00:49:17 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,13 +3,3 @@ npm install
 
 echo "Setting up required folders..."
 mkdir public/styles
-
-echo "Removing unnecessary stuff..."
-rm .gitignore
-rm README.md
-rm -Rf .git
-
-echo "Building project..."
-cake build
-
-echo "Done! now run your project with 'node app' or 'cake run' and build something awesome!"
